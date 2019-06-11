7791159e8ea9f31e4a32d2125182f2259cfd1235
mozhanrong <mozhanrong@126.com>
2019-06-11 13:31:29 +0800

    初始化提交

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {  };
+  }
+  render() {
+    return (
+      <div>这是根组件</div>
+    );
+  }
+}
+
+export default App
